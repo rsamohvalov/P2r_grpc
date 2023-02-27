0f11eba76a65fd856d834cf908155a4383bb69c0
R.Samokhvalov <r.samokhvalov@kryptonite.ru>
2023-02-27 19:25:59 +0300

    id

--- P2r_example.c
+++ P2r_example.c
@@ -4,9 +4,9 @@
 ApiCallTable callbacks;
 
 int SendSpeedNotification(struct_SpeedNotification speed, void *context);
-void *RunClient(char *addr);
+void *RunClient(char *addr, int fp_id);
 
-int RunServer(char *addr, ApiCallTable *callbacks, int id );
+int RunServer(char *addr, ApiCallTable *callbacks, int id);
 
 void NotificationCallback(struct_SpeedNotification speed)
 {
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     callbacks.restore_callback = RestoreCallback;
     RunServer("0.0.0.0:50051", &callbacks, 1 );
 
-    void *context = RunClient("0.0.0.0:50051");
+    void *context = RunClient("0.0.0.0:50051", 1 );
     if (!context)
     {
         return -1;
